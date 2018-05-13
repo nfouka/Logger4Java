ccb5a0333b203015d5e8587f558234035fc007b1
nfouka <nadir.fouka@gmail.com>
2018-05-13 18:54:45 +0200

    first commit

---
+++ src/main/java/com/javacodegeeks/snippets/enterprise/log4jexample/Example.java
@@ -0,0 +1,20 @@
+package com.javacodegeeks.snippets.enterprise.log4jexample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.log4j.Logger;
+
+public class Example {
+
+	static Logger log = Logger.getLogger(Example.class.getName());
+
+	public static void main(String[] args) throws IOException{
+		
+		
+		log.debug("Number is smaller than 100, so it is correct!");
+		log.info("Number is smaller than 100, so it is correct!");
+		log.error("Number is smaller than 100, so it is correct!");
+	}
+}
